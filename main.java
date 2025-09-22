de31f2129d9ae9f5ff02ea893d74539c7816cbb1
iCopterr <zxcopterzx1@gmail.com>
2025-09-22 23:07:39 +0700

    Update main.java

--- main.java
+++ main.java
@@ -1,4 +1,4 @@
-public class Main {
+public class main {
     public static void main(String[] args) {
         Vehicle v = new Vehicle("2548", "iCopter", "Modelzaza007", 1998, "iCopterMotors", 25000000.0);
         v.displayInfo();
