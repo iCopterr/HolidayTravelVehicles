e953d42ef20a3c4fad8888496f7016a7f80bd33b
iCopterr <zxcopterzx1@gmail.com>
2025-09-22 22:59:21 +0700

    Create SaleInvoice.java

---
+++ SaleInvoice.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class SaleInvoice {
+    private String saleInvoiceNumber;
+    private String saleDate;
+    private Customer customer;
+    private Vehicle purchasedVehicle;
+    private TradeInVehicle tradeInVehicle;
+    private List<DealerOption> dealerOptions = new ArrayList<>();
+    private double totalPrice;
+    private double taxes;
+    private double licenseFees;
+
+    public SaleInvoice(String number, String date, Customer customer, Vehicle purchasedVehicle) {
+        this.saleInvoiceNumber = number;
+        this.saleDate = date;
+        this.customer = customer;
+        this.purchasedVehicle = purchasedVehicle;
+    }
+
+    public void addDealerOption(DealerOption option) {
+        dealerOptions.add(option);
+    }
+
+    public void recordTradeIn(TradeInVehicle tradeIn) {
+        this.tradeInVehicle = tradeIn;
+    }
+
+    public void calculateTotal() {
+        totalPrice =  0;
+        for (DealerOption option : dealerOptions) {
+            totalPrice += option.calculateOptionCost();
+        }
+        totalPrice += taxes + licenseFees;
+    }
+
+    public void printInvoice() {
+        System.out.println("Invoice No: " + saleInvoiceNumber);
+        System.out.println("Date: " + saleDate);
+        System.out.println("Customer: " + customer.getCustomerName());
+        purchasedVehicle.displayInfo();
+        System.out.println("Total: " + totalPrice);
+    }
+}
